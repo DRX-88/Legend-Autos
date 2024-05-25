de9921e6f6c7331436615f218971b3df06950c61
DRX-88 <nrmj02@gmail.com>
2024-05-25 19:57:40 -0400

    added to client side - pages, components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,3 +2,37 @@ import ReactDom from 'react-dom/client';
 import { createBrowerRouter, RouterProvider } from 'react-router-dom/dist';
 import 'index.css';
 
+import App from '.App.jsx';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from '/pages/Login';
+import Signup from '/pages/Signup';
+import Error from '/pages/Error';
+
+
+const router = createBrowerRouter(
+    [
+        {
+            path: '/',
+            element: <App />,
+            error: <Error />,
+            children: [
+                { index: true,
+                    element: <Home />
+                },
+                {
+                    path: '/profile',
+                    element: <Profile />
+                },
+                {
+                    path: '/login',
+                    element: <Login />
+                },
+                {
+                    path: '/signup',
+                    element: <Signup />
+                }
+            ]
+        }
+    ]
+);
